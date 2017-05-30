3d98bd9388d4be5c604b990e3f82f5a892489ac4
zydhmily <zydhmily0214@gmail.com>
2017-05-30 22:02:07 +0900

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,17 +1,18 @@
 public class Card{
-	String rank;
-	String suit;
+    String rank;
+    String suit;
+	
     public Card(String rank,String suit){
     	this.rank = rank;
     	this.suit = suit;
     }
     
     //得到扑克牌牌面
-	public String getCard(){
-		if(rank != " "){
+    public String getCard(){
+	if(rank != " "){
             return rank + " of " + suit;
         }else{
-        	return suit;
+            return suit;
         }
-	}
+    }
 }
