609c838c426cd93f3ba36d116dcec01d2b0d51d3
zydhmily <zydhmily0214@gmail.com>
2017-06-14 23:01:53 +0900

    Create JokerCard.java

--- JokerCard.java
+++ JokerCard.java
@@ -1,9 +1,9 @@
 public class JokerCard extends Card{
-	private String joker;
+    private String joker;
 	
-	public JokerCard(String joker){
-	    this.joker = joker;
-	}
+    public JokerCard(String joker){
+	this.joker = joker;
+    }
 
     //得到扑克牌牌面
     public String getFace(){
