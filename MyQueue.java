a4f5d111023e2373e5388c0303093e42136d2917
zydhmily <zydhmily0214@gmail.com>
2017-06-18 23:04:17 +0900

    栈和队列

--- MyQueue.java
+++ MyQueue.java
@@ -1,16 +1,17 @@
 import java.util.*;
 
 public class MyQueue{
+	//定义一个存储元素的链表
 	private LinkedList list = new LinkedList();
-
+    //向队列中放入元素
 	public void put(Object o){
 	    list.addLast(o);
 	}
-
+    //从队列取得元素
 	public Object get(){
         return list.removeFirst();
 	}
-
+    //检查队列是否为空
 	public boolean isEmpty(){
         return list.isEmpty();
 	}
