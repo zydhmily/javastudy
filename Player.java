32efe235bcdb0a20e63192324fe898966444d888
zydhmily <zydhmily0214@gmail.com>
2017-07-03 21:48:12 +0900

    注释追加

--- Player.java
+++ Player.java
@@ -8,15 +8,15 @@ public class Player{
     public Player(int id){
     	this.id = id;   	
     }
-
+    //向玩家手里添加卡牌
     public void putCard(Card card){
     	playerCards.add(card);
     }
-
+    //取得玩家手里指定的牌面
     public String getPlayerCardFace(int index){
         return playerCards.get(index).getFace();
     }
-
+    //取得玩家手牌数
     public int getPlayerCardNumber(){
     	return playerCards.size();
     }
--- PokerGame.java
+++ PokerGame.java
@@ -18,21 +18,21 @@ public class PokerGame{
     //洗牌2
 	public void shuffle2(Deck deck){
 		Calendar cld = Calendar.getInstance();
-		//毫秒
+		//取得系统时间的毫秒
 		int MI = cld.get(Calendar.MILLISECOND);
 		//毫秒后两位
-		int MI2 = Integer.parseInt(String.valueOf(MI).substring(2));
+		int MIAfterTwo = Integer.parseInt(String.valueOf(MI).substring(1));
 		if(MI > 10){
-			for(int i = 1;i <= MI2*10;i++){
+			for(int i = 1;i <= MIAfterTwo*10;i++){
 				shuffle1(deck);
 			}
 		}else{
-			for(int i = 1;i <= MI2;i++){
+			for(int i = 1;i <= MIAfterTwo;i++){
                 shuffle1(deck);
             }
 		}
 	}
-
+    //向玩家发牌
 	public void deal(List<Player> players,Deck deck){
 		while(deck.getCardNumber() > 0){
 		    for(int i = 0;i < players.size();i++){
@@ -46,11 +46,15 @@ public class PokerGame{
 		PokerGame game = new PokerGame();
 		List<Player> players = new ArrayList<Player>();  
 		Deck deck = new Deck();
+		//向玩家列表添加玩家
 		players.add(new Player(1));
 		players.add(new Player(2));
 		players.add(new Player(3));
+		//有洗牌2方法洗牌
 		game.shuffle2(deck);
+		//向玩家发牌
 		game.deal(players,deck);
+		//打印玩家手牌的牌面
 		System.out.println("玩家：" + players.get(0).id + "的手牌");
 		for(int i = 0;i < players.get(0).getPlayerCardNumber();i++){
 			System.out.println(players.get(0).getPlayerCardFace(i));
