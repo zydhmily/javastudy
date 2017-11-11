b135e20592a894e6d403d79dd18fa8fa33a16034
zydhmily <zydhmily0214@gmail.com>
2017-11-11 21:56:33 +0900

    修正

--- javastudy/io/FileCopy.java
+++ javastudy/io/FileCopy.java
@@ -5,26 +5,30 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileCopy {
+	//缓冲区大小
+	private static final int BLOCK_SIZE = 4096;
+
 	public static void main(String[] args) {
-		//缓冲区大小
-		int bufferValue = 4096;
 		//声明输入流对象
 		FileInputStream input = null;
 		//声明输出流对象
 		FileOutputStream output = null;
+
 		try {
 			//给输入流对象赋予入力文件path
 			input = new FileInputStream(args[0]);
 			//给输出流对象赋予出力文件path
 			output = new FileOutputStream(args[1]);
+
 			//声明一个缓冲区数组变量
-			byte[] buffer = new byte[bufferValue];
+			byte[] bytes = new byte[BLOCK_SIZE];
 			//声明记录读入字节数变量
-			int byteCnt = 0;
+			int totalNumber;
+			
 			//循环读入文件内容
-			while ((byteCnt = input.read(buffer, 0, bufferValue)) != -1) {
+			while ((totalNumber = input.read(bytes)) != -1) {
 				//输出读入内容到输出流对象
-				output.write(buffer, 0, byteCnt);
+				output.write(bytes, 0, totalNumber);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -39,8 +43,7 @@ public class FileCopy {
 					output.close();
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("无法关闭输入输出流，请联系系统管理员");
+
 			}
 		}
 	}
